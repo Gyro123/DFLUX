6394d076d246e404e4a9c797acee92b560d6de15
Gyro123 <110230071+Gyro123@users.noreply.github.com>
2022-07-30 09:24:59 +0800

    Update gaosimianreyuan.for

--- 20220729/gaosimianreyuan.for
+++ 20220729/gaosimianreyuan.for
@@ -4,7 +4,8 @@ C
       INCLUDE 'ABA_PARAM.INC'
       DIMENSION COORDS(3),FLUX(2),TIME(2)
       CHARACTER*80 SNAME
-C
+C     user coding to define FLUX(1) and FLUX(2)
+C     高斯热源参数
       Q=1000
       v=50
       Rh=0.005
@@ -17,7 +18,7 @@ C
       z0=0
 C
       PI=3.1415
-C
+C     焊件上任意一点距离热源中心的距离
       R=SQRT((x-x0-d))**2+(y-y0)**2)
 C
       FLUX(1)=3*Q/(PI*Rh**2)*EXP(-3*R**2/Rh**2)
